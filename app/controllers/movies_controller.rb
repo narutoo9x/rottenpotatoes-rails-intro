b436f0a315c41f6db405bf4b2b1eb6eb67c96ae2
Narutoo9x <kingkid162@gmail.com>
2016-05-17 03:39:01 +0700

    hw2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,21 +11,68 @@ class MoviesController < ApplicationController
   end
 
   def index
-    #sort movies by movie title and movie release date
-    @movies = Movie.order(params[:sort_by])
-    # if use below statement will be error:
-    #Couldn't find all Movies with 'id': (all, {:order=>nil}) (found 0 results, but was looking for 2)
-    #@movies = Movie.find(:all, :order => (params[:sort_by]))
-    if params[:ratings]
-      #@movies = Movie.where(:rating => params[:ratings].keys).find(:all, :order => (params[:sort_by]))
-      @movies = Movie.where(:rating => params[:ratings].keys).order(params[:sort_by])
-    end
-    @sort_column = params[:sort_by]
     @all_ratings = Movie.all_ratings
-    @set_ratings = params[:ratings]
-    if !@set_ratings
-      @set_ratings = Hash.new
+    redirect = false
+    #The logger is useful to save infomation to log files at runtime
+    logger.debug(session.inspect) #New Session #{@session.arttributes.inspect}
+
+    # sort_by
+    if params[:sort_by]
+      @sort_by = params[:sort_by]
+      session[:sort_by] = params[:sort_by]
+    elsif session[:sort_by]
+      @sort_by = session[:sort_by]
+      redirect = true
+    else
+      @sort_by = nil
     end
+    # ratings
+    if params[:commit] == "Refresh" and params[:ratings].nil?
+      @ratings = nil
+      session[:ratings] = nil
+    elsif params[:ratings]
+      @ratings = params[:ratings]
+      session[:ratings] = params[:ratings]
+    elsif session[:ratings]
+      @ratings = session[:ratings]
+      redirect = true
+    else
+      @ratings = nil
+    end
+
+    if redirect
+      flash.keep
+      redirect_to movies_path :sort_by => @sort_by, :ratings => @ratings
+    end
+
+    # @sort_by = params[:sort_by]
+    # @ratings = params[:ratings]
+
+    if @ratings and @sort_by
+      @movies = Movie.where(:rating => @ratings.keys).order(@sort_by)
+    elsif @ratings
+      @movies = Movie.where(:rating => @ratings.keys)
+    elsif @sort_by
+      @movies = Movie.order(@sort_by)
+    else
+      @movies = Movie.all
+    end
+
+    if !@ratings
+      @ratings = Hash.new
+    end
+
+    # @movies = Movie.all
+    #
+    # if params[:ratings]
+    #   @movies = Movie.where(:rating => @ratings.keys).order(@sort_by)
+    # end
+    # @sort_column = params[:sort_by]
+    # @all_ratings = Movie.all_ratings
+    # @set_ratings = params[:ratings]
+    # if !@set_ratings
+    #   @set_ratings = Hash.new
+    # end
   end
 
   def new
